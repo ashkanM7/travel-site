ab2ba1ecdbacd68967e1e6034774140684e86c04
Ashkan <ashkan.yaghmorlou@gmail.com>
2020-07-14 14:44:24 -0400

    Completed desktop and mobile header styles

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,8 @@
 import "../styles/styles.css";
+import MobileMenue from "./modules/MobileMenu";
+let mobileMenu = new MobileMenue();
 
 if(module.hot){
     module.hot.accept();
 }
+ 
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const path = require("path")
      require("postcss-mixins"),
      require("postcss-simple-vars"),
      require("postcss-nested"),
+     require("postcss-hexrgba"),
      require("autoprefixer")
  ]
 
