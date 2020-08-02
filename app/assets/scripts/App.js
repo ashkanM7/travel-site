2f5223f5cd78ea1af172b3e9e60a0d841a5c2e08
Ashkan <ashkan.yaghmorlou@gmail.com>
2020-08-01 22:00:18 -0400

    Completed sticky header functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,9 @@
 import "../styles/styles.css";
 import MobileMenue from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/revealOnScroll";
+import StickyHeader from "./modules/StickyHeader"
+
+let stickyHeader = new StickyHeader();
 let mobileMenu = new MobileMenue();
 new RevealOnScroll(document.querySelectorAll(".feature-item"),75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 65);
